043f52dbd4f3b1848aff7f28ce99008b8c7ab9a2
student31415 <qwerty65k@mail.ru>
2019-11-21 10:02:58 +0300

    main prog and report

--- main.cpp
+++ main.cpp
@@ -1,74 +1,99 @@
 #include<iostream>
 #include<stdlib.h>
-struct data{
-	double key;
-	char str[64];
+#include<chrono>
+const int SIZE_OF_STRING=65;
+const int STARTING_SIZE=100;
+const int MAX_VALUE=200;
+const double SHIFT=100.0;
+struct Data{
+    double key;
+    char str[SIZE_OF_STRING];
 };
 
-class vector{
-	public:	
-		data * arr;
-		int size;
-		int max_size;
-		vector(){
-			size=0;
-			max_size=1;
-			arr = (data*) malloc(sizeof(data));
-		}
-		vector(int n){
-			size=0;
-			max_size=n;
-			arr = (data*) malloc(sizeof(data)*n);
-		}
-		~vector(){
-			free(arr);
-		}
-		void assing(int n){
-			max_size=n;
-			arr = (data*) realloc(arr, sizeof(data)*n);
-		}
-		data& operator [] (int indx){
-			return arr[indx];
-		}
+class TVector{
+    public:
+        Data * Arr;
+        int Size;
+        int MaxSize;
+        TVector(){
+            Size=0;
+            MaxSize=1;
+            Arr = ( Data * ) malloc( sizeof( Data ) );
+        }
+        TVector(int n){
+            Size=0;
+            MaxSize=n;
+            Arr = ( Data * ) malloc( sizeof( Data ) * n );
+        }
+        ~TVector(){
+            free(Arr);
+        }
+        void Assing(int n){
+            MaxSize=n;
+            Arr = ( Data* ) realloc(Arr, sizeof( Data ) * n );
+        }
+        Data& operator [] (int indx){
+            return Arr[indx];
+        }
 };
 
-void bucket_sort(vector& arr, int size){
-	vector* buckets=new vector[21];
-	for(int i=0; i<21; ++i) buckets[i].assing(size);
-	for(int i=0; i<size; ++i){
-		buckets[(int)(arr[i].key+100.0)/10][buckets[(int)(arr[i].key+100.0)/10].size++]=arr[i];
-	}	
-	int k;
-	for(int i=0; i<21; ++i){
-		if(buckets[i].size==0) continue;
-		for(int j=0; j<buckets[i].size; ++j){
-			data q=buckets[i][j];	
-			k=j;
-			for(k=j; k>0; --k){
-				if(q.key<buckets[i][k-1].key) buckets[i][k]=buckets[i][k-1]; else break;
-			}
-			buckets[i][k]=q;
-		}
-	}
-	k=0;
-	for(int i=0; i<21; ++i){
-		for(int j=0; j<buckets[i].size; ++j){ 
-			arr[k++]=buckets[i][j];
-		} 
-	}
-	delete [] buckets;
-	return;
+void BucketSort(TVector& arr) {
+    TVector* buckets=new TVector[arr.Size+1];
+    for (int i=0; i<arr.Size; ++i) {
+        if (buckets[ (int) ( (arr[i].key+SHIFT) / MAX_VALUE * arr.Size ) ].Size == buckets[ (int) ( ( arr[i].key+SHIFT) / MAX_VALUE * arr.Size ) ].MaxSize) {
+            buckets[ (int) ( (arr[i].key+SHIFT) / MAX_VALUE * arr.Size ) ].Assing( buckets[ (int) ( (arr[i].key+SHIFT) / MAX_VALUE * arr.Size ) ].Size * 2 );
+        }
+        buckets[ (int) ( (arr[i].key+SHIFT) / MAX_VALUE * arr.Size ) ][ buckets[ (int) ( (arr[i].key+SHIFT) / MAX_VALUE * arr.Size ) ].Size++ ]=arr[i];
+    }    
+    int k;
+    for (int i=0; i<arr.Size+1; ++i) {
+        if (buckets[i].Size==0 || buckets[i].Size==1) {
+            continue;
+        }
+        for (int j=0; j<buckets[i].Size; ++j) {
+            Data q=buckets[i][j];
+            k=j-1;
+            while (k>=0 && q.key<buckets[i][k].key) {
+                buckets[i][k+1]=buckets[i][k];
+                k-=1;
+            }
+            buckets[i][k+1]=q;
+        }
+    }
+    k=0;
+    for (int i=0; i<arr.Size+1; ++i) {
+        for (int j=0; j<buckets[i].Size; ++j) { 
+            arr[k++]=buckets[i][j];
+        } 
+    }
+    delete [] buckets;
+    return;
 }
 
-int main(){
-	vector arr(20);
-	int n=0;
-	while(std::cin){
-		std::cin>>arr[n].key>>arr[n].str; n++;
-	}
-	n-=1;
-	bucket_sort(arr, n);
-	for(int i=0; i<n; ++i){
-		std::cout<<arr[i].key<<"\t"<<arr[i].str<<std::endl;
-	}
+int main() {
+    std::ios::sync_with_stdio(false);
+    TVector arr(STARTING_SIZE);
+    while (std::cin) {
+        std::cin >> arr[ arr.Size ].key;
+        if (!std::cin) {
+            break;
+        }
+        std::cin>>arr[ arr.Size ].str; 
+        arr.Size+=1;
+        if (arr.Size==arr.MaxSize) {
+            arr.Assing( arr.Size * 2 );
+        }
+    }
+    std::cout<<"star sorting"<<std::endl;
+    std::chrono::time_point<std::chrono::system_clock> start, end;
+    start = std::chrono::system_clock::now();
+    BucketSort(arr);
+    end = std::chrono::system_clock::now();
+    
+  /*  for(int i=0; i<arr.Size; ++i){
+        std::cout<<std::fixed<<arr[i].key<<"\t"<<arr[i].str<<std::endl;
+    }*/
+    double diff=std::chrono::duration_cast<std::chrono::milliseconds> (end-start).count();
+    std::cout<<"sorting time = "<<std::fixed<<diff/1000<<"s"<<std::endl;
+    return 0;
 }
